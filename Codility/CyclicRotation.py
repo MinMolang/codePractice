8bb24f5fdc5b5772610e3aedfa8779a9bf9588a8
MinMolang <pc5432cmsknr@gmail.com>
2020-01-22 19:55:47 +0900

    add link , In history,There are 75 87 100 answers

--- Codility/CyclicRotation.py
+++ Codility/CyclicRotation.py
@@ -1,3 +1,5 @@
+#https://app.codility.com/programmers/lessons/2-arrays/cyclic_rotation/ "codility CyclicRotation link"
+#In history,There are 75 87 100 answers 
 def solution(A, K):
     # write your code in Python 3.6
     if not A:
