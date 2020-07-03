5409783347dd98c7bb2c0191b072afae523a7c1e
MinMolang <pc5432cmsknr@gmail.com>
2020-07-03 16:24:13 +0900

    update

--- BOJ/BOJ11003(SlidingWindow).py
+++ BOJ/BOJ11003(SlidingWindow).py
@@ -1,20 +1,26 @@
 # https://www.acmicpc.net/problem/11003
-# 최솟값찾기
-
+# 최소값찾기
+# 덱 오름차준으로 저장
+# (인덱스, 값)
+# 맨 앞 인덱스와 현재 L 만큼 차이나는지 확인
+# 맨 뒤에 있는 값이 현재 값보다 크면 삭제하고 현재 값을 덱에 넣는다.
 import sys
 ans = ''
 n,l = map(int,sys.stdin.readline().strip().split())
 a =  list(map(int,sys.stdin.readline().strip().split()))
 al = len(a)
-for k in range(1,al+1):
-   if k-l+1<=0:
-       first = 0
-   else:
-       first = k-l+1-1
-   last = k
-   ans+=str(min(a[first:last]))
+dq = [] #덱 역할
+for k in range(al):
+   cur = a[k]
+   if dq and dq[0][1]<=k-l:
+       dq.pop(0)
+   while dq and dq[-1][0]>cur:
+       dq.pop()
+   dq.append((cur,k))
+   ans+=str(dq[0][0])
    ans+=' '
 print(ans[:-1])
+
 
 
 
