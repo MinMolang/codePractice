f18b96e216758e98490ee6aa228765a128dfe38f
MinMolang <pc5432cmsknr@gmail.com>
2020-07-03 16:37:53 +0900

    update

--- BOJ/BOJ11003(SlidingWindow).py
+++ BOJ/BOJ11003(SlidingWindow).py
@@ -4,16 +4,17 @@
 # (인덱스, 값)
 # 맨 앞 인덱스와 현재 L 만큼 차이나는지 확인
 # 맨 뒤에 있는 값이 현재 값보다 크면 삭제하고 현재 값을 덱에 넣는다.
+import collections
 import sys
 ans = ''
 n,l = map(int,sys.stdin.readline().strip().split())
 a =  list(map(int,sys.stdin.readline().strip().split()))
 al = len(a)
-dq = [] #덱 역할
+dq = collections.deque([])
 for k in range(al):
    cur = a[k]
    if dq and dq[0][1]<=k-l:
-       dq.pop(0)
+       dq.popleft()
    while dq and dq[-1][0]>cur:
        dq.pop()
    dq.append((cur,k))
