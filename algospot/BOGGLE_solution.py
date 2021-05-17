49f805bb20cc567ddb8d2875eeed7438d7ccd3b9
MinMolang <pc5432cmsknr@gmail.com>
2021-05-18 04:10:23 +0900

    Update BOGGLE_solution.py
    시간초과..

--- algospot/BOGGLE_solution.py
+++ algospot/BOGGLE_solution.py
@@ -1,72 +1,40 @@
-# 1
-# URLPM
-# XPRET
-# GIAET
-# XTNZY
-# XOQRS
-# 6
-# PRETTY
-# GIRL
-# REPEAT
-# KARA
-# PANDORA
-# GIAZAPX
+xy = [
+    (-1, -1),
+    (0, -1),
+    (1, -1),
+    (-1, 0),
+    (1, 0),
+    (-1, 1),
+    (0, 1),
+    (1, 1),
+]
 
-# PRETTY YES
-# GIRL YES
-# REPEAT YES
-# KARA NO
-# PANDORA NO
-# GIAZAPX YES
-
-# 실패한 예제
-# 1
-# NNNNN
-# NEEEN
-# NEYEN
-# NEEEN
-# NSNNN
-# 1
-# YES
+#코드출처: https://doriri.tistory.com/33 [My Programming]
 
 
-# TODO
-# board 받아오기 ok
-# 테스트케이스 받아오기 ok
-# board 5*5 돌면서 최초글자 해당하는지 확인ok
-# board 재귀호출하며 단어가 있는지 확인 
-dx = [-1, -1, -1, 1, 1, 1, 0, 0]
-dy = [-1, 0, 1, -1, 0, 1, -1, 1]
+def find_word_st(dx, dy, test_case, index):
+    for (x, y) in xy:
+        if dy + y > 4 or dy + y < 0 or dx + x > 4 or dx + x < 0:
+            continue
+
+        if board[dy + y][dx + x] is test_case[index]:
+            if length is index + 1:
+                return True
+            if find_word_st(dx + x, dy + y, test_case, index + 1) is True:
+                return True
 
 
-def hasWord(y, x, test_case):
-    #print(y,x)
-    # 기저사례1. 시작 위치가 범위 밖이면 무조건 실패
-    if y < 0  or y > 4 or x < 0 or x > 4:
-      #print('t1')
-      return False
-    
-    boolean = (test_case[0]==board[y][x])
-    test_case = test_case[1:]
-    
-    if boolean:
-      if not(len(test_case)):
-        # 빈경우
-        #print('t2')
-        return True
-      else:
-        #print('t4')
-        #인접한 여덟 칸을 검사한다.
-        for k in range(8):
-          #다이나믹 프로그래밍
-            nx = x + dx[k]
-            ny = y + dy[k]
-        
-            if hasWord(ny, nx, test_case):
-              return True
-    else:
-      #print('t5')
-      return False
+def find_word_ch(test_case):
+    global length
+    length = len(test_case)
+
+    for dy in range(0, 5):
+        for dx, ch in enumerate(board[dy]):
+            if test_case[0] is ch:
+                if find_word_st(dx, dy, test_case, 1) is True:
+                    return "YES"
+
+    return "NO"
 
 
 c = int(input())
@@ -78,17 +46,5 @@ for _ in range(c):
     n = int(input())
     for _ in range(n):
         test_case = input()
-        
-        ans = False
-        
-        for r in range(5):
-          for c in range(5):
-             ans = hasWord(r, c, test_case)
-             if ans:
-               break
-          if ans:
-            break
-          
-            
-        
-        print(test_case, 'YES' if ans else 'NO')
+
+        print(test_case, find_word_ch(test_case))
