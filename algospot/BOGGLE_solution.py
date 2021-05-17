0f44447e1551c49ced12e1f651dad52293eee369
MinMolang <pc5432cmsknr@gmail.com>
2021-05-18 03:34:40 +0900

    예제 성공, 시간초과

--- algospot/BOGGLE_solution.py
+++ algospot/BOGGLE_solution.py
@@ -40,30 +40,42 @@ dy = [-1, 0, 1, -1, 0, 1, -1, 1]
 
 
 def hasWord(y, x, test_case):
-    print(y,x)
+    #print(y,x)
     # 기저사례1. 시작 위치가 범위 밖이면 무조건 실패
-    if not(x >= 0 and x < 5) or not(y >= 0 and y < 5):
-      print('1')
+    if y < 0  or y > 4 or x < 0 or x > 4:
+      #print('t1')
+      return False
+    
+    # 기저사례2. 첫글자가 일치하지 않으면 실패
+    if board[y][x] != test_case[0]:
+      #print('bo',board[y][x])
+      #print('tc',test_case[0])
+      #print('t2')
       return False
       
-    # 기저사례2. 첫 글자가 일치하지 않으면 실패
-    if board[y][x] != test_case[0]:
-      print('2')
-      return False
-    
     # 기저사례3. 단어길이가 1이면 성공
-    if len(test_case)==1:
+    if len(test_case) == 1:
+      #print('t3')
       return True
+      
+    #boolean = (test_case[0]==board[y][x])
+    #test_case = test_case[1:]
     
-    #인접한 여덟 칸을 검사한다.
+    #if boolean:
+    #  if not(len(test_case)):
+        # 빈경우
+     #   print('t2')
+      #  return True
+      #else:
+    #print('t4')
+        #인접한 여덟 칸을 검사한다.
     for k in range(8):
         nx = x + dx[k]
         ny = y + dy[k]
         
         if hasWord(ny, nx, test_case[1:]):
-          print('3')
           return True
-          
+    #print('t5')
     return False
 
 
@@ -78,10 +90,15 @@ for _ in range(c):
         test_case = input()
         
         ans = False
+        
         for r in range(5):
           for c in range(5):
-              ans = hasWord(r,c,test_case)
-              if ans == True:
-                break
+             ans = hasWord(r, c, test_case)
+             if ans:
+               break
+          if ans:
+            break
+          
+            
         
-        print(ans)
+        print(test_case, 'YES' if ans else 'NO')
