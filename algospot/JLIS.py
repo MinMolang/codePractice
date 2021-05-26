d27d0fdf59d236a05f3a24f0da8b66bf757bb5ca
MinMolang <pc5432cmsknr@gmail.com>
2021-05-26 22:36:30 +0900

    Update JLIS.py
    #py3는 시간초과나서 pypy2로 패스

--- algospot/JLIS.py
+++ algospot/JLIS.py
@@ -1,3 +1,5 @@
+#py3는 시간초과나서 pypy2로 
+
 # 입력
 # 3
 # 3 3
@@ -15,54 +17,45 @@
 # 6
 # 5
 
-# 코드 출처 : https://junho-one.tistory.com/11
+# 코드 출처 : only123님 풀이
+
 
 import sys
 
-
-def Jlis(posA, posB):  #
-    # dp에 존재하는 지 확인
-    if dp[posA][posB]:  # -1이 아니면 lis를 구한 것이므로 길이를 return
-        return dp[posA][posB]
-
-    ret = 2  # sequenceA[posA], sequenceB[posB]가 항상 존재하므로 2개는 항사 있다
-
-    currA, currB = sequenceA[posA], sequenceB[posB]
-
-    # A, B 둘 중에 큰 숫자 찾기
-    maxElement = max(currA, currB)
-
-    # 다음 원소 찾기
-    for nextA in range(posA + 1, lis_lenA):
-        if maxElement < sequenceA[nextA]:
-            ret = max(ret, Jlis(nextA, posB) + 1)
-
-    for nextB in range(posB + 1, lis_lenB):
-        if maxElement < sequenceB[nextB]:
-            ret = max(ret, Jlis(posA, nextB) + 1)
-
-    dp[posA][posB] = ret  # dp에 저장 후 리턴
-    return ret
+MIN = -3000000000
 
 
-c = int(input())
-for i in range(c):
-    lis_len = list(map(int, sys.stdin.readline().strip().split()))
-    lis_lenA, lis_lenB = lis_len[0], lis_len[1]
-    sequenceA = list(map(int, sys.stdin.readline().rstrip().split()))
-    sequenceB = list(map(int, sys.stdin.readline().rstrip().split()))
+def Jlis():
+    for ai in reversed(xrange(-1, lis_lenA)):
+        for bi in reversed(xrange(-1, lis_lenB)):
+            a = MIN if ai == -1 else sequenceA[ai]
+            b = MIN if bi == -1 else sequenceB[bi]
+            maxVal = max(a, b)
 
-    # 음수정수값 추가
-    # 함수 자체가 다음값을 구해나가는 과정이 있어서
-    sequenceA.insert(0, -sys.maxsize + 1)
-    sequenceB.insert(0, -sys.maxsize + 1)
+            ret = 0
+            for i in xrange(ai + 1, lis_lenA):
+                if maxVal < sequenceA[i]:
+                    val = dp[i + 1][bi + 1] + 1
+                    ret = max(ret, val)
+            for i in xrange(bi + 1, lis_lenB):
+                if maxVal < sequenceB[i]:
+                    val = dp[ai + 1][i + 1] + 1
+                    ret = max(ret, val)
+            dp[ai + 1][bi + 1] = ret
+    return dp[0][0]
 
-    # 음수정수값 추가한만큼 길이 추가
-    lis_lenA += 1
-    lis_lenB += 1
 
-    # 다이나믹 프로그래밍 메모이제이션
-    dp = [[None for _ in range(lis_lenB)] for _ in range(lis_lenA)]
+rl = lambda: sys.stdin.readline()
 
-    # 더해준 2개 다시 빼주기
-    print(Jlis(0, 0) - 2)
+for _ in xrange(input()):
+    lis_lenA, lis_lenB = map(int, rl().split())
+    sequenceA = map(int, rl().split())
+    sequenceB = list(map(int, rl().split()))
+
+    #다이나믹 메모이제이션션
+   dp = [[0] * (lis_lenB + 1) for _ in xrange(lis_lenA + 1)]
+
+    print
+    Jlis()
+
+
