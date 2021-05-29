be812a715944cc67d33dee5dea4b28ef0ddc6a8d
MinMolang <pc5432cmsknr@gmail.com>
2021-05-29 17:43:26 +0900

    Update PI.py
    이것도 시간초과..

--- algospot/PI.py
+++ algospot/PI.py
@@ -18,68 +18,78 @@
 # 두 개의 숫자가 번갈아 가며 출현할 때 (예: 323, 54545) 난이도: 4
 # 숫자가 등차 수열을 이룰 때 (예: 147, 8642) 난이도: 5
 # 그 외의 경우 난이도: 10
+
+# 풀이 출처 : doctidea님 풀이
 import  sys
 
 sys.setrecursionlimit(10**6)
-INF = 987654321
 
-# numbers [a .. b] 구간의 난이도를 반환
-def classify(a, b):
+# numbers [a,b,c] 세자리 구간의 난이도를 반환
+def score_3(a, b, c):
     # 숫자 조각을 가져온다
-    sub = numbers[a, b+1]
-    # 첫 글자만으로 이루어진 문자열과 같으면 난이도 1
-    if len(set(sub)) == 1:
-        return  1
-
-    progressive = True
-    for i in range(len(sub) - 1):
-        if (sub[i+1] - sub[i]) != (sub[1] - sub[0]):
-            progressive = False
-            break
-
-    # 단조증가, 감소, = 등차수열이고 공차가 1 또는 -1이면 난이도 2
-    if progressive and abs(sub[1] - sub[0]) == 1:
+    if a == b == c: # 모든 숫자 같음
+        return 1
+    if a-b == b-c == 1 or a-b == b-c == -1: #단조증가/감소
         return 2
-
-    # 두 수가 번갈아 등장하는지 확인
-    alternating = True
-    for i in range(len(sub)):
-        if sub[i] != sub[i % 2]: # 리스트 괄호 오타
-            alternating  = False
-            break #추가
-
-    if alternating:
+    if a==c!=b: # 숫자가 번갈아가면서 출현
         return 4
-    if progressive:
+    if a-b == b-c: # 등차수열
         return 5
+    return  10
 
-    return 10
+# numbers [a,b,c,d] 네자리 구간의 난이도를 반환
+def score_4(a, b, c, d):
+    # 숫자 조각을 가져온다
+    if a == b == c == d: # 모든 숫자 같음
+        return 1
+    if a-b == b-c == c-d  == 1 or a-b == b-c == c-d ==  -1: #단조증가/감소
+        return 2
+    if a == c and b == d and a!=b: # 숫자가 번갈아가면서 출현
+        return 4
+    if a-b == b-c == c-d: # 등차수열
+        return 5
+    return  10
+
+# numbers [a,b,c,d,e] 다섯자리 구간의 난이도를 반환
+def score_5(a, b, c, d,e):
+    # 숫자 조각을 가져온다
+    if a == b == c == d == e: # 모든 숫자 같음
+        return 1
+    if a-b == b-c == c-d == d-e == 1 or a-b == b-c == c-d == d-e ==  -1: #단조증가/감소
+        return 2
+    if a == c == e and b == d and a != b: # 숫자가 번갈아가면서 출현
+        return 4
+    if a-b == b-c == c-d == d-e: # 등차수열
+        return 5
+    return  10
 
 
-# 시작하는 인덱스
-def memorize(begin):
+def memorize(numbers):
+    N = len(numbers)
+    dp = [None]*(N+1)
+    dp[3] = score_3(numbers[0], numbers[1], numbers[2]) # 3개까지 읽음
+    dp[4] = score_4(numbers[0], numbers[1], numbers[2], numbers[3]) # 4개까지 읽음
+    dp[5] = score_5(numbers[0], numbers[1], numbers[2], numbers[3], numbers[4]) #5개까지 읽음
+    for i in range(6, N+1):
+        cand = []
+        if dp[i-3] is not None:
+            cand.append(dp[i-3] + score_3(numbers[i-3], numbers[i-2], numbers[i-1]))
+        if dp[i-4] is not None:
+            cand.append(dp[i-4] + score_4(numbers[i-4], numbers[i-3], numbers[i-2], numbers[i-1]))
+        if dp[i-5] is not None:
+            cand.append(dp[i-5] + score_5(numbers[i-5], numbers[i-4], numbers[i-3], numbers[i-2], numbers[i-1]))
 
-    # 기저사례 : 수열의 끝에 도달했을 경우
-    if begin == len(numbers):
-        return 0
+        dp[i] = min(cand) # 최소값을 다시 메모이제이션
+    return dp[-1] #마지막 값을 제공
 
-    # 메모이제이션
-    ret = dp[begin]
-    if ret != -1: # -1 초기값인지, 이미 구했는지 확인
-        return ret
 
-    ret = INF
-    # 세자리수, 다섯자리 수 판단
-    for num in [3, 4, 5]:
-        if begin + num <= len(numbers):
-            ret = min (ret, memorize(begin + num) + classify(begin, begin + num - 1)) # 구하지 않은 값이미면 최소값 구하기
 
-    return ret
+
 
 for _ in range(int(input())):
     numbers = list(map(int, list(input().strip())))
     dp = [-1] * len(numbers)
-    print(memorize(0))
+    print(memorize(numbers))
 
 
 
