33d1ff8bb74d1cbd9ab82d740f40c14fcd869ec9
MinMolang <pc5432cmsknr@gmail.com>
2021-06-02 06:36:10 +0900

    Update TRIANGLEPATH.py

--- algospot/TRIANGLEPATH.py
+++ algospot/TRIANGLEPATH.py
@@ -1,4 +1,28 @@
+# 2
+# 5
+# 6
+# 1  2
+# 3  7  4
+# 9  4  1  7
+# 2  7  5  9  4
+# 5
+# 1
+# 2 4
+# 8 16 8
+# 32 64 32 64
+# 128 256 128 256 128
+#
+# 28
+# 341
 
+# 6
+# 1  2
+# 3  7  4
+# 9  4  1  7
+# 2  7  5  9  4
+# 의 경우 6 + 2 + 4 + 7 + 9
+
+# 풀이 참고 https://hellominchan.tistory.com/239
 import sys
 
 input = sys.stdin.readline
@@ -16,3 +40,6 @@ for _ in range(int(input())):
                 board[i][j] += max(board[i - 1][j], board[i - 1][j - 1])
 
     print(max(board[-1]))
+
+
+
