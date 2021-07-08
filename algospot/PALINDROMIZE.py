14fd30e8783fa5cd382a19c1ba35a6f60e719263
MinMolang <pc5432cmsknr@gmail.com>
2021-07-09 06:36:35 +0900

    Add files via upload

---
+++ algospot/PALINDROMIZE.py
@@ -0,0 +1,26 @@
+# # 입력
+# 3
+# there
+# amanaplanacanal
+# xyz
+#
+# # 출력
+# 7
+# 21
+# 5
+
+# original의 접미사이면서 reversed_str의 접두사인 문자열 길이의 최대길이를 구한다
+
+def maxOverlap(original, reversed_str):
+
+for _ in range(int(input())):
+    original = input()
+    len_original = len(original)
+
+    reversed_str = ''
+    for i in range(len_original-1, -1, -1):
+        reversed_str += original[i]
+
+    # 핵심! 원래 문자열과 뒤집은 문자열을 합친 뒤, Suffix와 Prefix끼리 겹치는 만큼 빼면 가장 짧은 문자열
+
+    print(len_original * 2 - maxOverlap(original, reversed_str))
